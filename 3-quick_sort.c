66bd27483029f60748ea2e4538dae30e2db6cdb1
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-12-19 00:55:35 +0200

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,33 +1,55 @@
 #include "sort.h"
 /**
  * partitioning - function that partitioning an array
- * @arr: the array
+ * @array: the array
+ * @size: the array size
+ * @start: the start of partitioning
+ * @end: end of partitioning
  * Return: the pivot position
 */
-size_t partitioning(int *array, size_t start, size_t end)
+size_t partitioning(int *array, size_t size, size_t start, size_t end)
 {
-    int pivot = array[end - 1], temp;
-    size_t p_index = start, i;
+	int pivot = array[end], temp;
+	size_t p_index = start, i;
 
-    if (end <= start)
-    {
-        return (0);
-    }
-    for (i = start ; i <= end - 2 ; i++)
-    {
-        if (array[i] <= pivot)
-        {
-            temp = array[i];
-            array[i] = array[p_index];
-            array[p_index] = temp;
-            p_index++;
-        }
-    }
-    array[end - 1] = array[p_index];
-    array[p_index] = pivot;
-    print_array(array, end);
-    return p_index;
+	for (i = start ; i < end ; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			temp = array[i];
+			array[i] = array[p_index];
+			array[p_index] = temp;
+			p_index++;
+		}
+	}
+	array[end] = array[p_index];
+	array[p_index] = pivot;
+	print_array(array, size);
+	return (p_index);
 }
+
+/**
+ * quicksort - function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ * @arr: the array
+ * @size: the array size
+ * @start: the start of partitioning
+ * @end: end of partitioning
+*/
+void quicksort(int arr[], size_t size, size_t start, size_t end)
+{
+	size_t p_index;
+
+	if (start < end)
+	{
+	p_index = partitioning(arr, size, start, end);
+
+		quicksort(arr, size, start, p_index - 1);
+		quicksort(arr, size, p_index + 1, end);
+
+	}
+}
+
 
 /**
  * quick_sort - function that sorts an array of integers in
@@ -37,15 +59,7 @@ size_t partitioning(int *array, size_t start, size_t end)
 */
 void quick_sort(int *array, size_t size)
 {
-    size_t low = 0;
-    size_t high = size - 1;
-    size_t p_index;
-
-    while (high > low)
-    {
-        p_index = partitioning(array, low, high);
-        quick_sort(array, p_index + 1);
-        high = p_index - 1;
-    }
-    
+	if (!array || !size)
+		return;
+	quicksort(array, size, 0, size - 1);
 }
